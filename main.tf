4907d8bb282cfe63b155c962a44332d0614d8c5d
Josevaldo <juniorjosevaldo1@gmail.com>
2023-01-20 09:00:35 -0300

    teste user_data

--- main.tf
+++ main.tf
@@ -10,6 +10,7 @@ terraform {
 }
 
 provider "aws" {
+ 
   region  = "us-west-2"
 }
 
@@ -17,8 +18,17 @@ resource "aws_instance" "app_server" {
   ami           = "ami-04bad3c587fe60d89"
   instance_type = "t2.micro"
   key_name = "JoseAWS-SSH"
+  user_data_replace_on_change = true
+  user_data = <<-EOF
+                #!/bin/bash
+                cd /home/ubuntu
+                mkdir testeScript
+                cd ~/testeScript
+                echo "<h1>TesteScript</h1>" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
 
   tags = {
-    Name = "testeTerraform"
+    Name = "erraformScript"
   }
 }
